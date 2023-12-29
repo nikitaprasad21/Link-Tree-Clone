38f80c771ef43484aacb95f97d3eeac11eaea7f5
Nikita Prasad <84131752+nikitaprasad21@users.noreply.github.com>
2023-12-29 12:39:45 +0530

    Update linktree_clone.py

--- linktree_clone.py
+++ linktree_clone.py
@@ -15,7 +15,7 @@ col2.image(image, width=180)
 
 
 # User name and small description
-st.title("Hi-ya!!!👋, I'm Nikita Prasad")
+st.title("Hi-ya!!!👋, \n I'm Nikita Prasad")
 st.write("Data Analyst | Machine Learning and Data Analytics Practitioner")
 
 icon_size = 20
